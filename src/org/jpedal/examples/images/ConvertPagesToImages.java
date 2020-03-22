4223ed557afc953e5d0021b1e6c15d3c0d96a0ad
Kevin Goodwin <fwmechanic@gmail.com>
2020-03-22 09:22:58 -0700

    fix error similar to "JPEGImageWriter is internal proprietary API..."
    https://stackoverflow.com/a/34921002

--- src/org/jpedal/examples/images/ConvertPagesToImages.java
+++ src/org/jpedal/examples/images/ConvertPagesToImages.java
@@ -72,6 +72,7 @@ import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageTypeSpecifier;
 import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
 import javax.imageio.metadata.IIOMetadata;
 import javax.imageio.plugins.jpeg.JPEGImageWriteParam;
 import javax.imageio.stream.ImageOutputStream;
@@ -86,8 +87,6 @@ import org.jpedal.io.JAIHelper;
 import org.jpedal.objects.PdfFileInformation;
 import org.jpedal.utils.LogWriter;
 import org.w3c.dom.Element;
-
-import com.sun.imageio.plugins.jpeg.JPEGImageWriter;
 
 public class ConvertPagesToImages {
 
@@ -775,7 +774,7 @@ public class ConvertPagesToImages {
 		// com.sun.image.codec.jpeg.JPEGEncodeParam jpegEncodeParam = jpegEncoder.getDefaultJPEGEncodeParam(image_to_save);
 
 		// Image writer
-		JPEGImageWriter imageWriter = (JPEGImageWriter) ImageIO.getImageWritersBySuffix("jpeg").next();
+		ImageWriter imageWriter = ImageIO.getImageWritersBySuffix("jpeg").next();
 		ImageOutputStream ios = ImageIO.createImageOutputStream(fos);
 		imageWriter.setOutput(ios);
 
